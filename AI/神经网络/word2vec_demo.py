eb3c002f75109fd8dc2646b53307064d1f4093af
looken1024 <looken1024@163.com>
2025-12-27 17:31:39 +0800

    feat: 初始化

---
+++ AI/神经网络/word2vec_demo.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import jieba
+import word2vec
+
+# 读取数据
+dataframe = pd.read_csv('word2vec_input.csv', encoding='utf-8')
+# 对文本列进行分词
+dataframe['分词结果'] = dataframe['文本列'].apply(lambda x: ' '.join(jieba.cut(x)))
+# 保存为word2vec需要的格式
+dataframe['分词结果'].to_csv('word2vec_jieba_output.txt', index=False, header=False)
+
+# 训练模型
+word2vec.word2vec(train='word2vec_jieba_output.txt', 
+                  output='word2vec_model.bin', 
+                  size=100,  # 词向量维度
+                  window=5,  # 上下文窗口大小
+                  negative=5,  # 负采样数
+                  threads=4,  # 线程数
+                  iter_=5)    # 迭代次数
+
